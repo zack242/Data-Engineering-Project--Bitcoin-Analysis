bd3e672b399c7116b520315ed026d2c2b731bfe4
zack242 <zakaria_009@hotmail.fr>
2024-07-31 23:11:53 +0100

    add filter by date

--- DAG/dags/dbt/bitcoin_analysis/models/staging/stg_transactions.sql
+++ DAG/dags/dbt/bitcoin_analysis/models/staging/stg_transactions.sql
@@ -12,13 +12,13 @@ select
     ingestion_timestamp,
     '{{invocation_id}}' as batch_id
 from {{source('bitcoin_src','transactions')}}
-
+where date = '{{ var('cut_off_date' ) }}'
 
 {% if is_incremental() %}
 
   -- this filter will only be applied on an incremental run
   -- (uses >= to include records whose timestamp occurred since the last run of this model)
   -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
-where ingestion_timestamp > (select coalesce(max(ingestion_timestamp),'1900-01-01') from {{ this }} )
+and ingestion_timestamp > (select coalesce(max(ingestion_timestamp),'1900-01-01') from {{ this }} )
 
 {% endif %}
